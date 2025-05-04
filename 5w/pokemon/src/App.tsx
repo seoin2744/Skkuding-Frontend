821c832d1909868e9899566122f30b8550616fb1
seoin2744 <seoin9398@gmail.com>
2025-05-05 01:34:44 +0900

    5w

---
+++ 5w/pokemon/src/App.tsx
@@ -0,0 +1,27 @@
+import PokemonList from './components/PokemonList';
+import PokemonDetail from './components/PokemonDetail';
+import pokemons from './data/data';
+import './styles/App.css';
+import './styles/reset.css';
+import './styles/style.css';
+import usePokemonStore from './store/usePokemonStore';
+
+function App() {
+  const selectedPokemon = usePokemonStore((state) => state.selectedPokemon);
+  const setSelectedPokemon = usePokemonStore((state) => state.setSelectedPokemon);
+
+  return (
+    <>
+      <nav>
+        <h1 onClick={() => setSelectedPokemon(-1)}>Pokemon List</h1>
+      </nav>
+        {selectedPokemon === -1 ? (
+            <PokemonList pokemons={pokemons} onSelect={setSelectedPokemon} />
+          ) : (
+            <PokemonDetail pokemon={{ ...pokemons[selectedPokemon], id: selectedPokemon + 1 }} onBack={() => setSelectedPokemon(-1)} />
+        )}
+    </>
+  );
+}
+
+export default App;
---
+++ 5w/pokemon/src/store/usePokemonStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+interface PokemonStore {
+  selectedPokemon: number;
+  setSelectedPokemon: (index: number) => void;
+}
+
+const usePokemonStore = create<PokemonStore>((set) => ({
+  selectedPokemon: -1,
+  setSelectedPokemon: (index) => set({ selectedPokemon: index }),
+}));
+
+export default usePokemonStore;
